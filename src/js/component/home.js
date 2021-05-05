b4d78354c218085c4061ed4a86c0904e1aaab838
Martin Luaces <martinluaces17@gmail.com>
2021-05-05 15:08:08 +0000

    casi casi

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,147 +1,26 @@
 import React, { useRef, useEffect, useState } from "react";
 
 export function Home() {
-	const [songList, setSongList] = useState([
-		{
-			id: 1,
-			category: "game",
-			name: "Mario Castle",
-			url: "files/mario/songs/castle.mp3"
-		},
-		{
-			id: 2,
-			category: "game",
-			name: "Mario Star",
-			url: "files/mario/songs/hurry-starman.mp3"
-		},
-		{
-			id: 3,
-			category: "game",
-			name: "Mario Overworld",
-			url: "files/mario/songs/overworld.mp3"
-		},
-		{
-			id: 4,
-			category: "game",
-			name: "Mario Stage 1",
-			url: "files/mario/songs/stage1.mp3"
-		},
-		{
-			id: 5,
-			category: "game",
-			name: "Mario Stage 2",
-			url: "files/mario/songs/stage2.mp3"
-		},
-		{
-			id: 6,
-			category: "game",
-			name: "Mario Star",
-			url: "files/mario/songs/starman.mp3"
-		},
-		{
-			id: 7,
-			category: "game",
-			name: "Mario Underworld",
-			url: "files/mario/songs/underworld.mp3"
-		},
-		{
-			id: 8,
-			category: "game",
-			name: "Mario Underwater",
-			url: "files/mario/songs/underwater.mp3"
-		},
-		{
-			id: 9,
-			category: "game",
-			name: "Zelda Castle",
-			url: "files/videogame/songs/zelda_castle.mp3"
-		},
-		{
-			id: 10,
-			category: "game",
-			name: "Zelda Outworld",
-			url: "files/videogame/songs/zelda_outworld.mp3"
-		},
-		{
-			id: 11,
-			category: "game",
-			name: "Zelda Titles",
-			url: "files/videogame/songs/zelda_title.mp3"
-		},
-		{
-			id: 11,
-			category: "game",
-			name: "Sonic Brain Zone",
-			url: "files/videogame/songs/sonic_brain-zone.mp3"
-		},
-		{
-			id: 11,
-			category: "game",
-			name: "Zelda Link To Past",
-			url: "files/videogame/songs/zelda_link-to-past.mp3"
-		},
-		{
-			id: 12,
-			category: "game",
-			name: "Dong KinKong Main",
-			url: "files/other/songs/dkng-main.mp3"
-		},
-		{
-			id: 13,
-			category: "game",
-			name: "Dong KinKong Other",
-			url: "files/other/songs/dkng-other.mp3"
-		},
-		{
-			id: 14,
-			category: "game",
-			name: "mega-man",
-			url: "files/other/songs/mega-man.mp3"
-		},
-		{
-			id: 15,
-			game: "cartoon",
-			name: "Flintstones",
-			url: "files/cartoons/songs/flintstones.mp3"
-		},
-		{
-			id: 16,
-			game: "cartoon",
-			name: "power-rangers",
-			url: "files/cartoons/songs/power-rangers.mp3"
-		},
-		{
-			id: 17,
-			game: "cartoon",
-			name: "simpsons",
-			url: "files/cartoons/songs/simpsons.mp3"
-		},
-		{
-			id: 18,
-			game: "cartoon",
-			name: "south-park",
-			url: "files/cartoons/songs/south-park.mp3"
-		},
-		{
-			id: 19,
-			game: "cartoon",
-			name: "thundercats",
-			url: "files/cartoons/songs/thundercats.mp3"
-		},
-		{
-			id: 20,
-			game: "cartoon",
-			name: "x-men",
-			url: "files/cartoons/songs/x-men.mp3"
+	const [songList, setSongList] = useState([]);
+
+	useEffect(() => {
+		obtenerLista();
+	}, []);
+
+	const obtenerLista = async () => {
+		try {
+			const res = await fetch(
+				"https://assets.breatheco.de/apis/sound/songs"
+			);
+			const data = await res.json();
+			setSongList(data);
+		} catch (error) {
+			console.log(error);
 		}
-	]);
+	};
 
 	let music = useRef();
 
-	const reproducirMusic = () => {
-		music.current.src =
-			"https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3";
-	};
 	const playPause = () => {
 		if (music.current.paused) {
 			music.current.play();
@@ -152,17 +31,24 @@ export function Home() {
 
 	return (
 		<div>
-			{songList.map((objeto, index) => {
-				return (
-					<div key={index} onClick={() => {}}>
-						<span>{objeto.id}</span>
-						<span>{objeto.name}</span>
-					</div>
-				);
-			})}
+			<div id="sidebar">
+				<img src="http://assets.stickpng.com/thumbs/5ece4ff9123d6d0004ce5f89.png" />
+			</div>
+			<div id="content">
+				{songList.map((objeto, index) => {
+					return (
+						<div className="song margin-bottom" key={index}>
+							<div onClick={() => {}}>
+								<span>{objeto.id}</span>
+								{" - "}
+								<span>{objeto.name}</span>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 			<div
-				className="btn-group text-center"
-				role="group"
+				className="btn-group text-center playbar margin-left"
 				aria-label="Button group with nested dropdown">
 				<button type="button" className="btn btn-secondary">
 					<i className="fas fa-fast-backward"></i>
@@ -181,11 +67,7 @@ export function Home() {
 						className="dropdown-menu"
 						aria-labelledby="btnGroupDrop1">
 						<a className="dropdown-item" href="#">
-							<audio
-								ref={music}
-								src="https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3"
-								controls
-							/>
+							<audio ref={music} src="" controls />
 						</a>
 					</div>
 				</div>
