bd3d6d8d50dac19d796d7ec14503a1df678698a8
Martin Luaces <martinluaces17@gmail.com>
2021-05-05 20:17:43 +0000

    reproductor final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,19 +1,19 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 export function Home() {
-	const [songList, setSongList] = useState([]);
+	let [songList, setSongListAsync] = useState([]);
 
 	useEffect(() => {
-		obtenerLista();
+		obtenerList();
 	}, []);
 
-	const obtenerLista = async () => {
+	const obtenerList = async () => {
 		try {
 			const res = await fetch(
 				"https://assets.breatheco.de/apis/sound/songs"
 			);
 			const data = await res.json();
-			setSongList(data);
+			setSongListAsync(data);
 		} catch (error) {
 			console.log(error);
 		}
@@ -21,12 +21,46 @@ export function Home() {
 
 	let music = useRef();
 
+	const [isPlaying, setPlaying] = useState(false);
+
 	const playPause = () => {
 		if (music.current.paused) {
 			music.current.play();
+
+			setPlaying(true);
 		} else if (!music.current.paused) {
 			music.current.pause();
+			setPlaying(false);
 		}
+	};
+
+	// CAMBIAR CANCION
+
+	const [songActual, setSongActual] = useState();
+
+	const prevSong = () => {
+		let prev = songActual - 1;
+		if (prev <= 0) {
+			prev = songList.length - 1;
+		}
+		cambiarSrc(songList[prev].url, prev);
+	};
+	const nextSong = () => {
+		let next = songActual + 1;
+		if (next > songList.length - 1) {
+			next = 0;
+		}
+		cambiarSrc(songList[next].url, next);
+	};
+
+	//SRC AUDIO
+
+	const cambiarSrc = (url, song) => {
+		let string = "https://assets.breatheco.de/apis/sound/";
+		music.current.src = string + url;
+		setSongActual(song);
+		music.current.play();
+		setPlaying(true);
 	};
 
 	return (
@@ -37,40 +71,38 @@ export function Home() {
 			<div id="content">
 				{songList.map((objeto, index) => {
 					return (
-						<div className="song margin-bottom" key={index}>
-							<div onClick={() => {}}>
-								<span>{objeto.id}</span>
-								{" - "}
+						<div
+							className="song"
+							key={index}
+							onClick={() => {
+								cambiarSrc(objeto.url, index);
+							}}>
+							<div>
+								<span>
+									{objeto.id}
+									{" - "}
+								</span>
 								<span>{objeto.name}</span>
 							</div>
 						</div>
 					);
 				})}
 			</div>
-			<div
-				className="btn-group text-center playbar margin-left"
-				aria-label="Button group with nested dropdown">
-				<button type="button" className="btn btn-secondary">
-					<i className="fas fa-fast-backward"></i>
+			<audio ref={music} src={songList.url}></audio>
+			<div className="btn-group text-center playbar margin-left">
+				<button className="btn  btn-sm p-0" onClick={prevSong}>
+					<i className="fas fa-caret-square-left fa-2x boton" />
 				</button>
-				<button
-					type="button"
-					className="btn btn-secondary"
-					onClick={playPause}>
-					<i className="far fa-play-circle"></i>
+				<button className="btn  btn-sm p-0" onClick={playPause}>
+					{isPlaying ? (
+						<i className="fas fa-pause fa-2x boton"></i>
+					) : (
+						<i className="fas fa-play fa-2x boton"></i>
+					)}
 				</button>
-				<button type="button" className="btn btn-secondary">
-					<i className="fas fa-fast-forward"></i>
+				<button className="btn  btn-sm p-0" onClick={nextSong}>
+					<i className="fas fa-caret-square-right fa-2x boton" />
 				</button>
-				<div className="btn-group" role="group">
-					<div
-						className="dropdown-menu"
-						aria-labelledby="btnGroupDrop1">
-						<a className="dropdown-item" href="#">
-							<audio ref={music} src="" controls />
-						</a>
-					</div>
-				</div>
 			</div>
 		</div>
 	);
